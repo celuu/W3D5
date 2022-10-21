e4b6be79ba8842bde281512792b19e01b8ccf920
celuu <christine.e.luu@gmail.com>
2022-10-21 15:34:09 -0700

    up to build_move_tree

--- knights_travails/knight.rb
+++ knights_travails/knight.rb
@@ -3,8 +3,8 @@ require_relative 'polytreenode.rb'
 class KnightPathFinder
 
     def initialize(pos)
-        @root_node = PolyTreeNode.new(pos)
-        @considered_positions = [@root_node]
+        @start_pos = pos
+        @considered_positions = [@start_pos]
     end
 
     def self.valid_moves(pos) #[2,2], our current position
@@ -18,10 +18,42 @@ class KnightPathFinder
         return potential_moves.select{|p| p[0]>=0 && p[0] <= 7 && p[1]>=0 && p[1] <= 7} 
     end
 
-    def find_path(pos)
-
+    def new_move_positions(pos)
+        potential_moves = KnightPathFinder.valid_moves(pos)
+        @considered_positions.each do |p|
+            if potential_moves.include?(p)
+                potential_moves.delete(p)
+            end
+        end
+        @considered_positions.concat(potential_moves)
+        return potential_moves
     end
+
+    def build_move_tree(start_pos)
+        queue = new_move_positions(start_pos)
+        until queue.empty?
+            position = queue.shift
+            position.each do |child|
+                queue << child
+            end
+        end
+    end
+
+    # def bfs(target)
+    #     queue = [self]
+    #     until queue.empty?
+    #         element = queue.shift
+    #         return element if element.pos == target
+    #         element.children.each do |child|
+    #             queue << child
+    #         end
+    #     end
+    #     nil
+    # end
+
+
+
+    attr_reader :start_pos
 end
 
 k = KnightPathFinder.new([2,2])
-# p k.valid_moves([7,7])
