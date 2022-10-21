23018dab7eefe567d9ddd5cc0207fe1b16e86fa1
celuu <christine.e.luu@gmail.com>
2022-10-21 13:39:42 -0700

    finished up to dfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -12,4 +12,28 @@ class PolyTreeNode
         @parent = node
         node.children << self if !node.children.include?(self)
     end
+
+    def add_child(node)
+        node.parent=(self)
+        @children << node if !@children.include?(node)
+    end
+
+    def remove_child(node)
+        if !@children.include?(node)
+            raise "node is not a child, can't remove"
+        end
+        node.parent=(nil)
+        @children.delete(node)
+    end
+
+    def dfs(target)
+        return nil if self.nil?
+        return self if self.value == target
+
+        self.children.each do |child|
+            result = child.dfs(target)
+            return result unless result.nil?
+        end
+        return nil
+    end
 end
