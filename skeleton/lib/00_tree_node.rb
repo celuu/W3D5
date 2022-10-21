c5556f8ecb8341b927559bccbe61a3f25e8c7019
Patrick Choi <ptrckchoi@gmail.com>
2022-10-21 12:01:07 -0700

    initialize, parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+    attr_reader :value, :parent, :children
 
+    def parent=(node)
+        return @parent=nil if node.nil?
+        @parent.children.delete(self) if @parent
+        @parent = node
+        node.children << self if !node.children.include?(self)
+    end
 end
